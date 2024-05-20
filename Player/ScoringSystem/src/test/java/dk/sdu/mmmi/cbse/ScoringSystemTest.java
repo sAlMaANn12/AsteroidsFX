becd55c128ce04bc4fecabe62192153d2e2846f1
SaAli22 <116625689+SaAli22@users.noreply.github.com>
2024-05-20 22:43:43 +0200

    added microservice

---
+++ Player/ScoringSystem/src/test/java/dk/sdu/mmmi/cbse/ScoringSystemTest.java
@@ -0,0 +1,14 @@
+package dk.sdu.mmmi.cbse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ScoringSystemTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
